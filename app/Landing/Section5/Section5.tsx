da86d1ebbd532508c970d2446f3c63a431dd98ae
ZulkerQQ <zulker@quadque.tech>
2024-01-01 10:08:33 +0600

    !Update: Further Responsive section 5 and 6

--- app/Landing/Section5/Section5.tsx
+++ app/Landing/Section5/Section5.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const SectionFive = () => {
   return (
     <div className="lg:min-h-[40vh] min-h-[80vh] w-full flex items-center justify-center">
-      <div className="lg:grid lg:grid-cols-8 lg:gap-8 flex flex-col justify-center items-center lg:w-[69.5vw] w-[90vw] lg:min-h-[30vh] min-h-[80vh] lg:mt-0">
+      <div className="lg:grid lg:grid-cols-8 lg:gap-8 flex flex-col justify-center items-center lg:w-[69.5vw] w-[90vw] lg:min-h-[40vh] min-h-[80vh] lg:mt-0">
         <div className="col-span-4 flex flex-col lg:items-start items-center justify-center lg:min-h-full">
           <h6 className="text-[#4D8CFC] text-[32px] lg:w-4/5 w-[90vw] lg:text-start text-center leading-10">
             Never <span className="text-white text-[36px] font-semibold">Used CRM</span> & Not
