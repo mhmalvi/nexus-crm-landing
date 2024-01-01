6802687e5c13612c51e5dd7f26c603e9615191e0
ZulkerQQ <zulker@quadque.tech>
2024-01-01 10:11:43 +0600

    !Update: Section 10 margin bottom added

--- app/Landing/Section10/SectionTen.tsx
+++ app/Landing/Section10/SectionTen.tsx
@@ -4,7 +4,7 @@ import Logo from "../../../public/png/Q-icon.png";
 import { competitors } from "./TenData";
 const SectionTen = () => {
   return (
-    <div className="lg:min-h-[92vh] min-h-[90vh] w-full flex items-center justify-center">
+    <div className="lg:min-h-[92vh] min-h-[90vh] w-full flex items-center justify-center lg:mb-8">
       <div className="flex flex-col justify-center lg:w-[69.5vw] w-[90vw] lg:min-h-[92vh] min-h-[90vh]">
         <h6 className="text-[#4D8CFC] lg:text-[32px] lg:text-start text-center m-0">
           The best deal is here to
@@ -15,8 +15,8 @@ const SectionTen = () => {
         <p className="text-white lg:w-3/5 w-[90vw] lg:text-[1.2rem] text-base lg:text-start text-center mb-[10px] leading-10">
           Discover Unmatched Value: Compare Our Pricing and Features
         </p>
-        <div className="grid lg:grid-cols-2 grid-cols-1 gap-8">
-          <div className="flex flex-col justify-center items-center lg:border-[2px] border-b-[2px] border-[#4D8CFC] lg:rounded-xl drop-shadow-md lg:w-3/4 w-full">
+        <div className="grid lg:grid-cols-2 grid-cols-1 gap-8 ">
+          <div className="flex flex-col justify-center items-center lg:border-[2px] border-b-[2px] border-[#4D8CFC] lg:rounded-xl drop-shadow-md lg:w-3/4 lg:h-full w-full">
             <Image src={Logo} alt="logo" className="w-3/5" />
             <p className="lg:text-[2rem] text-white">$3.49/month</p>
           </div>
